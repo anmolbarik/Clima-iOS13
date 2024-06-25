139901ba1644f8cd13e3227408c978f7247015b4
Anmol Barik <30435955+anmolbarik@users.noreply.github.com>
2024-06-25 21:33:12 +0530

    key updated

--- Clima/WeatherManager.swift
+++ Clima/WeatherManager.swift
@@ -7,3 +7,24 @@
 //
 
 import Foundation
+
+let url = "https://api.openweathermap.org/data/2.5/weather?q=PURI&appid=fbe2a90982f29bb6c1196207eddf1ee6"
+
+func getWeather(urlString: String){
+    if let url = URL(string: urlString){
+        let session = URLSession(configuration: .default)
+        let task = session.dataTask(with: url, completionHandler: handler(data: response: error:))
+        task.resume()
+    }
+}
+
+func handler(data: Data?, response: URLResponse?, error: Error?){
+    if error != nil{
+        print(error!)
+        return
+    }
+    if let safeData = data {
+        let dataString = String(data: safeData, encoding: .utf8)
+        print(dataString!)
+    }
+}
